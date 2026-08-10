e2ff8ec8f3e56c7807bd83855a7dbdad4cee50f9
jiaweizhong <oracleinscience@gmail.com>
2026-08-09 21:34:20 -0700

    Add verytiny object loss

--- scripts/esod_baseline/tinyperson_eval/tinyperson_cocoeval.py
+++ scripts/esod_baseline/tinyperson_eval/tinyperson_cocoeval.py
@@ -634,7 +634,7 @@ class Params:
             if eval_standard == 'tiny': self.iouThrs = np.array([0.25, 0.5, 0.75])
             elif eval_standard == 'tiny_sanya17': self.iouThrs = np.array([0.3, 0.5, 0.75])
             else: raise ValueError("eval_standard is not right: {}, must be 'tiny' or 'tiny_sanya17'".format(eval_standard))
-            self.recThrs = np.linspace(.0, 1.00, np.round((1.00 - .0) / .01) + 1, endpoint=True)
+            self.recThrs = np.linspace(.0, 1.00, int(np.round((1.00 - .0) / .01)) + 1, endpoint=True)
             self.maxDets = [200]
             self.areaRng = [[1 ** 2, 1e5 ** 2], [1 ** 2, 20 ** 2], [1 ** 2, 8 ** 2], [8 ** 2, 12 ** 2],
                             [12 ** 2, 20 ** 2], [20 ** 2, 32 ** 2], [32 ** 2, 1e5 ** 2]]
@@ -646,8 +646,8 @@ class Params:
             self.imgIds = []
             self.catIds = []
             # np.arange causes trouble.  the data point on arange is slightly larger than the true value
-            self.iouThrs = np.linspace(.5, 0.95, np.round((0.95 - .5) / .05) + 1, endpoint=True)
-            self.recThrs = np.linspace(.0, 1.00, np.round((1.00 - .0) / .01) + 1, endpoint=True)
+            self.iouThrs = np.linspace(.5, 0.95, int(np.round((0.95 - .5) / .05)) + 1, endpoint=True)
+            self.recThrs = np.linspace(.0, 1.00, int(np.round((1.00 - .0) / .01)) + 1, endpoint=True)
             self.maxDets = [1, 10, 100]
             self.areaRng = [[0 ** 2, 1e5 ** 2], [0 ** 2, 32 ** 2], [32 ** 2, 96 ** 2], [96 ** 2, 1e5 ** 2]]
             self.areaRngLbl = ['all', 'small', 'medium', 'large']
@@ -667,8 +667,8 @@ class Params:
         self.imgIds = []
         self.catIds = []
         # np.arange causes trouble.  the data point on arange is slightly larger than the true value
-        self.iouThrs = np.linspace(.5, 0.95, np.round((0.95 - .5) / .05) + 1, endpoint=True)
-        self.recThrs = np.linspace(.0, 1.00, np.round((1.00 - .0) / .01) + 1, endpoint=True)
+        self.iouThrs = np.linspace(.5, 0.95, int(np.round((0.95 - .5) / .05)) + 1, endpoint=True)
+        self.recThrs = np.linspace(.0, 1.00, int(np.round((1.00 - .0) / .01)) + 1, endpoint=True)
         self.maxDets = [20]
         self.areaRng = [[0 ** 2, 1e5 ** 2], [32 ** 2, 96 ** 2], [96 ** 2, 1e5 ** 2]]
         self.areaRngLbl = ['all', 'medium', 'large']
