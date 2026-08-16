f3092533528526db40f25c1aba8c1aef913d1eb0
jiaweizhong <oracleinscience@gmail.com>
2026-08-16 09:42:56 -0700

    Add uncertainty aware gate

--- hesod/backends/hesod/train.py
+++ hesod/backends/hesod/train.py
@@ -250,7 +250,8 @@ def train(hyp, opt, device, tb_writer=None):
             optimizer.load_state_dict(ckpt['optimizer'])
             best_fitness = ckpt['best_fitness']
             if rank in [-1, 0]:
-                print('loaded optimizer, best_fitness = %.2f' % float(best_fitness))
+                bf = best_fitness.item() if hasattr(best_fitness, 'item') else best_fitness
+                print('loaded optimizer, best_fitness = %.2f' % bf)
 
         # EMA
         if opt.resume and ema and ckpt.get('ema'):
